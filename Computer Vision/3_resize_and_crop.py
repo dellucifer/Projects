a1844044b95292ab8333f748be7d904371d33c29
Dellucifer <84488210+dellucifer@users.noreply.github.com>
2022-10-19 09:37:19 +0530

    Update 3_resize_and_crop.py

--- Computer Vision/3_resize_and_crop.py
+++ Computer Vision/3_resize_and_crop.py
@@ -1,5 +1,7 @@
+# Importing OpenCV
 import cv2
 
+# Importing Image and configuring it
 img = cv2.imread('cards.jpeg')
 print(img.shape)
 
@@ -8,6 +10,7 @@ img_resize = cv2.resize(img,(width,height))
 print(img_resize.shape)
 img_crop = img[30:300,300:500]
 
+# Showing Image
 cv2.imshow('Imgae', img)
 # cv2.imshow('Resized_Image',img_resize)
 cv2.imshow('Cropped_Image',img_crop)
